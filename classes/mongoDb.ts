5ad941b74b0bda63dbaf53f1648816146f0233c2
Gabriel Montoya <gabi.montoya@gmail.com>
2021-04-07 22:55:13 -0300

    Se configuro la conexion a MongoDB

---
+++ classes/mongoDb.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+export default class MongoDb {
+    private static _instance: MongoDb;
+    private _connectionURL: string = 'mongodb://localhost:27017/appCursos';
+
+    private constructor() {
+    }
+
+    // Patron singleton
+    public static get instance() {
+        return this._instance || (this._instance = new this());
+    }
+
+    public connect(callback: any) {
+        mongoose.connect(
+            this._connectionURL,
+            {
+                useNewUrlParser: true,
+                useCreateIndex: true
+            },
+            callback);
+    }
+}
--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import Server from "./classes/server";
 import userRoutes from "./routes/usuarios";
 import MySqlDb from "./classes/mysqlDb";
+import MongoDb from "./classes/mongoDb";
+
 
 const server = Server.instance;
 server.start(() => {
@@ -10,14 +12,24 @@ server.start(() => {
 // Rutas de la app
 server.app.use('/users', userRoutes)
 
-
 // Conexion mySQL
 const mySqlDb: MySqlDb = MySqlDb.instance;
 mySqlDb.connect(((err: any) => {
     if(err){
-       console.error(err);
+       throw err;
     } else {
          console.log(`Conectado a la Base de datos MySql: ${ mySqlDb.host }:${ mySqlDb.port }\\${ mySqlDb.database}`);
      }
+}));
+
+
+// Conexion mongoose
+const mongoDb: MongoDb = MongoDb.instance;
+mongoDb.connect(((err: any) => {
+    if(err){
+        throw err;
+    } else {
+        console.log(`Conectado a MongoDb`);
+    }
 }));
 
